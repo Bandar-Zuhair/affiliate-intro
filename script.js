a3e7c86593d653d1451bf614ecd3b20dc6db81cd
Bandar-Zuhair <146974861+Bandar-Zuhair@users.noreply.github.com>
2024-10-27 01:21:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function openWhatsApp() {
+    const message = encodeURIComponent("Hi Richegoo! Any Available Website For Me to Start My Affiliate Marketing Business?");
+    const whatsappNumber = 'https://wa.me/+6282210081028?text=' + message; // Replace 1234567890 with your actual WhatsApp number
+    window.open(whatsappNumber, '_blank');
+}
