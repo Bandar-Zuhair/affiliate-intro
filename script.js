0c5b55e12f2f230ee4725270c130a4df9d0de1e7
Bandar-Zuhair <146974861+Bandar-Zuhair@users.noreply.github.com>
2024-11-07 03:31:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,34 @@ function openWhatsApp() {
     const whatsappNumber = 'https://wa.me/+6282210081028?text=' + message; // Replace 1234567890 with your actual WhatsApp number
     window.open(whatsappNumber, '_blank');
 }
+
+
+// JavaScript for Scroll Animations
+document.addEventListener("DOMContentLoaded", () => {
+    const animatedElements = document.querySelectorAll(".animate_on_scroll");
+
+    const observerOptions = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.1
+    };
+
+    const observerCallback = (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("intro-animation");
+                entry.target.classList.remove("outro-animation");
+            } else {
+                entry.target.classList.remove("intro-animation");
+                entry.target.classList.add("outro-animation");
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    animatedElements.forEach(element => {
+        observer.observe(element);
+    });
+});
+
