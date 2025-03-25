7b5a965f5435f69c2476e1b24956b1fabc4eba7d
Bandar <146974861+Bandar-Zuhair@users.noreply.github.com>
2025-03-26 01:31:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function openWhatsApp() {
     const message = encodeURIComponent("Hi Richegoo! Any Available Website For Me to Start My Affiliate Marketing Business?");
-    const whatsappNumber = 'https://wa.me/+6282210081028?text=' + message; // Replace 1234567890 with your actual WhatsApp number
+    const whatsappNumber = 'https://wa.me/+966569446280?text=' + message; // Replace 1234567890 with your actual WhatsApp number
     window.open(whatsappNumber, '_blank');
 }
 
